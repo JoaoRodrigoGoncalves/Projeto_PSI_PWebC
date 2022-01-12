3a9121d5c66f148d619fa630324c62eaf16c8486
Tiago <tiagoamaro021@gmail.com>
2022-01-12 15:50:25 +0000

    HomePage Design quase finalizado

--- js/funcoes_homepage.js
+++ js/funcoes_homepage.js
@@ -1,7 +1,21 @@
 'use strict';
 
+function mudarImagem(){
+    if( document.getElementById("myImg").src == "images/estrela_npre.png" ){
+        document.getElementById("myImg").src = "images/estrela_pre.png";
+    }
+    else if( document.getElementById("myImg").src == "images/estrela.png" ){
+        var resposta="Tem a certeza que deseja remover de favorito?";
+        confirm(resposta);
+    }
+}
 function apresentar_dados(dados)
 {
-    $('#nome').text(dados.data.id);
+    $.each(dados.data,function(index,value){
+        console.log(value);
+        var img_final = value.image.replace("large", "thumb");
+        $('#tabela_homepage > tbody').append('<tr><td><button onclick="mudarImagem()" style="border:none"><img id="imagem" src="images/estrela_npre.png"></button></td><td>' + value.market_cap_rank + '</td><td><img src="'+ img_final +'"></td><td>'+value.name+'</td><td>'
+        +value.symbol+'</td><td>'+value.current_price+'€</td><td>'+value.price_change_24h+'€('+value.price_change_percentage_24h+'%)</td></tr>');
+    })
 }
-obterMoeda('bitcoin', apresentar_dados);
+obterTop100(apresentar_dados);
--- js/script_favoritos.js
+++ js/script_favoritos.js
@@ -4,4 +4,4 @@ function apresentar_dados(dados)
 {
     $('#teste').text(dados.data.id);
 }
-obterMoeda('bitcoin', apresentar_dados);
+obterMoeda('nome', apresentar_dados);
