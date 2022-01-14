a76d9371e33da52c2ccc2a4f47030ff39d903bcd
Dr0Gh0st <bm39502@gmail.com>
2022-01-14 09:55:32 +0000

    Mais detalhes adicionados
    Preço atual e Alteração do preço total

--- js/funcao_detalhes.js
+++ js/funcao_detalhes.js
@@ -17,10 +17,15 @@ function detalhes(res){
     console.log(res.data.market_cap_rank);
     console.log(res.data.image.large);
     console.log(res.data.description.pt);
+    console.log(res.data.market_data.current_price.eur);
+    console.log(res.data.market_data.ath_change_percentage.eur);
+
 
     $('.nome', CloneMedia).text("Nome: " + res.data.name);
     $('.rank', CloneMedia).text("Rank no Mercado: " + res.data.market_cap_rank);
-    $('.desc', CloneMedia).html("Descrição: " + res.data.description.en)
+    $('.desc', CloneMedia).html("Descrição: " + res.data.description.en);
+    $('.precoAtual', CloneMedia).html("Preço Atual: " + res.data.market_data.current_price.eur);
+    $('.precoChance', CloneMedia).html("Alteração do Preço Total: " + res.data.market_data.ath_change_percentage.eur);
     $('#image', CloneMedia).attr('src', res.data.image.large);
     $('.link', CloneMedia).attr('href', res.data.links.homepage[0])
 
