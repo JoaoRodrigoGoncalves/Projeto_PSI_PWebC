7119907135e593df33b0fb567c67fdabe64d2c07
Dr0Gh0st <bm39502@gmail.com>
2022-01-14 10:29:07 +0000

    alteração de preço total para preço nas ultimas 24h

--- js/funcao_detalhes.js
+++ js/funcao_detalhes.js
@@ -18,14 +18,14 @@ function detalhes(res){
     console.log(res.data.image.large);
     console.log(res.data.description.pt);
     console.log(res.data.market_data.current_price.eur);
-    console.log(res.data.market_data.ath_change_percentage.eur);
+    console.log(res.data.market_data.price_change_24h);
 
 
     $('.nome', CloneMedia).text("Nome: " + res.data.name);
     $('.rank', CloneMedia).text("Rank no Mercado: " + res.data.market_cap_rank);
     $('.desc', CloneMedia).html("Descrição: " + res.data.description.en);
     $('.precoAtual', CloneMedia).html("Preço Atual: " + res.data.market_data.current_price.eur);
-    $('.precoChance', CloneMedia).html("Alteração do Preço Total: " + res.data.market_data.ath_change_percentage.eur);
+    $('.precoChance', CloneMedia).html("Alteração do Preço nas Ultimas 24h: " + res.data.market_data.price_change_24h);
     $('#image', CloneMedia).attr('src', res.data.image.large);
     $('.link', CloneMedia).attr('href', res.data.links.homepage[0])
 
