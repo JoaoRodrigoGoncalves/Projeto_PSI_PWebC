217fb61ed392d783a55c6187a816ee582b0f03e0
Tiago <tiagoamaro021@gmail.com>
2022-01-14 11:00:26 +0000

    Alterações da tabela e adição do footer

--- js/funcoes_homepage.js
+++ js/funcoes_homepage.js
@@ -14,8 +14,15 @@ function apresentar_dados(dados)
     $.each(dados.data,function(index,value){
         console.log(value);
         var img_final = value.image.replace("large", "thumb");
-        $('#tabela_homepage > tbody').append('<tr><td><button onclick="mudarImagem()" style="border:none"><img id="imagem" src="images/estrela_npre.png"></button></td><td>' + value.market_cap_rank + '</td><td><img src="'+ img_final +'"></td><td>'+value.name+'</td><td>'
+        $('#tabela_homepage > tbody').append('<tr><td>'+apresentarBotaoFavoritos(value.id)+'</td><td>' + value.market_cap_rank + '</td><td><img src="'+ img_final +'"></td><td>'+value.name+'</td><td>'
         +value.symbol+'</td><td>'+value.current_price+'€</td><td>'+value.price_change_24h+'€('+value.price_change_percentage_24h+'%)</td></tr>');
     })
 }
 obterTop100(apresentar_dados);
+
+function abrirModalRemocao(id)
+{
+    var modal = new bootstrap.Modal(document.getElementById("confirmacaoModal"));
+    $("#confirmarRemocaoBTN").attr("onclick", "removerFavoritos('" + id + "')");
+    modal.show();
+}
